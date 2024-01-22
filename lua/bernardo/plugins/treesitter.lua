9d4dfeeb9d8973caf86102a2cb73f9cccfa15beb
BernardoBF4 <bernardo.fantin@gmail.com>
2024-01-22 20:50:59 -0300

    ✨ feat(treesitter: treesitter): adicionado

--- lua/bernardo/plugins/treesitter.lua
+++ lua/bernardo/plugins/treesitter.lua
@@ -2,7 +2,17 @@ return {
   'nvim-treesitter/nvim-treesitter',
   config = function()
     require('nvim-treesitter.configs').setup({
-      ensure_installed = { 'javascript', 'typescript' },
+      ensure_installed = {
+        'javascript',
+        'typescript',
+        'php',
+        'html',
+        'lua',
+        'json',
+        'scss',
+        'css',
+        'sql',
+      },
       enable = true,
       highlight = {
         enable = true,
