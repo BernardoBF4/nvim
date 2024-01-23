38d61568ae920a1b206d3e8c7bb8a5147695736e
BernardoBF4 <bernardo.fantin@gmail.com>
2024-01-23 17:24:16 -0300

    🆙 improve(remaps: buffers, telescope, nvimtree): remaps melhorados

--- lua/bernardo/remap.lua
+++ lua/bernardo/remap.lua
@@ -1,5 +1,6 @@
 vim.g.mapleader = " "
 vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
+vim.keymap.set("n", "<C-s>", "<cmd>w<cr>")
 
 -- In normal mode, this allows me to move blocks of code up and down with J and K.
 vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
@@ -12,17 +13,14 @@ vim.keymap.set("n", "n", "nzzzv")
 vim.keymap.set("n", "N", "Nzzzv")
 
 -- Buffers
-vim.keymap.set("n", "\\]", "<cmd>bnext<cr>")
-vim.keymap.set("n", "\\[", "<cmd>bprev<cr>")
+vim.keymap.set("n", "<leader>.", "<cmd>bnext<cr>")
+vim.keymap.set("n", "<leader>,", "<cmd>bprev<cr>")
 
 -- Telescope
-vim.keymap.set("n", "<leader>ff", "<cmd>Telescope find_files<cr>")
-vim.keymap.set("n", "<leader>fg", "<cmd>Telescope git_files<cr>")
-vim.keymap.set("n", "<leader>fgs", "<cmd>Telescope grep_string<cr>")
-vim.keymap.set("n", "<leader>fgf", "<cmd>Telescope git_files<cr>")
+vim.keymap.set("n", "<leader>ff", "<cmd>Telescope git_files hidden=true show_untracked=true<cr>")
+vim.keymap.set("n", "<leader>fa", "<cmd>Telescope find_files<cr>")
 vim.keymap.set('n', '<leader>fb', '<cmd>Telescope buffers<cr>')
+vim.keymap.set("n", "<leader>fs", "<cmd>Telescope live_grep<cr>")
+
 -- NvimTree
-vim.keymap.set("n", "<leader>ee", "<cmd>NvimTreeToggle<CR>", { desc = "Toggle file explorer" })
-vim.keymap.set("n", "<leader>ef", "<cmd>NvimTreeFindFileToggle<CR>", { desc = "Toggle file explorer on current file" }) -- toggle file explorer on current file
-vim.keymap.set("n", "<leader>ec", "<cmd>NvimTreeCollapse<CR>", { desc = "Collapse file explorer" })                     -- collapse file explorer
-vim.keymap.set("n", "<leader>er", "<cmd>NvimTreeRefresh<CR>", { desc = "Refresh file explorer" })
+vim.keymap.set("n", "<leader>ee", "<cmd>NvimTreeToggle<cr>")
