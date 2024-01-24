2b52745c154ec9a30d105a50e54b70f4ea628a15
BernardoBF4 <bernardo.fantin@gmail.com>
2024-01-23 21:58:42 -0300

    🆙 improve(plugins: lsp): remaps feito

--- lua/bernardo/plugins/lsp-zero.lua
+++ lua/bernardo/plugins/lsp-zero.lua
@@ -54,10 +54,10 @@ return {
       },
       formatting = lsp_zero.cmp_format(),
       mapping = cmp.mapping.preset.insert({
-        ['<C-p>'] = cmp.mapping.select_prev_item(cmp_select),
-        ['<C-n>'] = cmp.mapping.select_next_item(cmp_select),
-        ['<C-y>'] = cmp.mapping.confirm({ select = true }),
-        ['<C-Space>'] = cmp.mapping.complete(),
+        ['<S-k>'] = cmp.mapping.select_prev_item(cmp_select),
+        ['<S-j>'] = cmp.mapping.select_next_item(cmp_select),
+        ['<S-l>'] = cmp.mapping.confirm({ select = true }),
+        ['<S-h>'] = cmp.mapping.complete(),
       }),
     })
   end
