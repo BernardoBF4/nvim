56d15e3b3dbfbabcfa5d0527a44509793a799ad1
BernardoBF4 <bernardo.fantin@gmail.com>
2024-01-26 17:48:54 -0300

    🆙 improve(plugin: lua line): mostrando o caminho completo do arquivo

--- lua/bernardo/plugins/lua-line.lua
+++ lua/bernardo/plugins/lua-line.lua
@@ -23,8 +23,8 @@ return {
       },
       sections = {
         lualine_a = { 'mode' },
-        lualine_b = { 'branch', 'buffers', 'diff', 'diagnostics' },
-        lualine_c = { 'filename' },
+        lualine_b = { 'branch', 'diff', 'diagnostics' },
+        lualine_c = { {'filename', path = 1} },
         lualine_x = { 'fileformat', 'filetype' },
         lualine_y = { 'progress' },
         lualine_z = { 'location' }
