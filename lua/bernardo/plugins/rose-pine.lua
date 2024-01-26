3fa7a4c941fde2c7106f18a8db55ee2bafedd992
BernardoBF4 <bernardo.fantin@gmail.com>
2024-01-25 23:32:04 -0300

    🆙 improve(temas: rosé pine): ajustado para minhas preferências

--- lua/bernardo/plugins/rose-pine.lua
+++ lua/bernardo/plugins/rose-pine.lua
@@ -1,4 +1,18 @@
 return {
   'rose-pine/neovim',
-  name = 'rose-pine'
+  name = 'rose-pine',
+  config = function ()
+   require('rose-pine').setup({
+     highlight_groups = {
+      ["String"] = { fg = "#27d653"},
+      ["@type.qualifier"] = { fg = "pine" },
+      ["@variable"] = { fg = "#ffffff" },
+      ["@variable.builtin"] = { fg = "#ffffff" },
+      ["@parameter"] = { fg = "gold" },
+      ["@property"] = { fg = "#ffffff" },
+      ["@new_expression"] = { fg = "pine" },
+      ["@named_imports"] = { fg = "#ffffff" },
+     }
+   })
+  end
 }
