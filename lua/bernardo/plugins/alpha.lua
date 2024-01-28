a897759c0456a15b89044a7988cabc4e5f422f47
BernardoBF4 <bernardo.fantin@gmail.com>
2024-01-28 17:54:30 -0300

    🆙 improve(plugins: alpha): melhor configurado

--- lua/bernardo/plugins/alpha.lua
+++ lua/bernardo/plugins/alpha.lua
@@ -8,6 +8,15 @@ return {
 
     -- Set header
     dashboard.section.header.val = {
+      "                                                                                   ",
+      "                                                                                   ",
+      "                                                                                   ",
+      "                                                                                   ",
+      "                                                                                   ",
+      "                                                                                   ",
+      "                                                                                   ",
+      "                                                                                   ",
+      "                                                                                   ",
       "████████╗ ██████╗ ████████╗██╗   ██╗███████╗    ████████╗██╗   ██╗██╗   ██╗███████╗",
       "╚══██╔══╝██╔═══██╗╚══██╔══╝██║   ██║██╔════╝    ╚══██╔══╝██║   ██║██║   ██║██╔════╝",
       "   ██║   ██║   ██║   ██║   ██║   ██║███████╗       ██║   ██║   ██║██║   ██║███████╗",
@@ -17,13 +26,8 @@ return {
     }
 
     -- Set menu
-    dashboard.section.buttons.val = {
-      dashboard.button("e", "  > New File", "<cmd>ene<CR>"),
-      dashboard.button("SPC ee", "  > Toggle file explorer", "<cmd>NvimTreeToggle<CR>"),
-      dashboard.button("SPC ff", "󰱼  > Find File", "<cmd>Telescope find_files<CR>"),
-      dashboard.button("SPC fs", "  > Find Word", "<cmd>Telescope live_grep<CR>"),
-      dashboard.button("q", "  > Quit NVIM", "<cmd>qa<CR>"),
-    }
+    dashboard.section.buttons.val = {}
+
 
     -- Send config to alpha
     alpha.setup(dashboard.opts)
