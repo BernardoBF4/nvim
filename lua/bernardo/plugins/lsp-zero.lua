2b4a89b1f0bd735c9bd4b2e0e53f9b0adcb232a1
BernardoBF4 <bernardo.fantin@gmail.com>
2024-01-28 17:26:30 -0300

    👘 style(plugins: lsp zero): formatado corretamente

--- lua/bernardo/plugins/lsp-zero.lua
+++ lua/bernardo/plugins/lsp-zero.lua
@@ -31,7 +31,7 @@ return {
     -- read this: https://github.com/VonHeikemen/lsp-zero.nvim/blob/v3.x/doc/md/guides/integrate-with-mason-nvim.md
     require('mason').setup({})
     require('mason-lspconfig').setup({
-      ensure_installed = { 'tsserver', 'volar', 'html', 'intelephense', 'cssls', 'jsonls', 'lua_ls'},
+      ensure_installed = { 'tsserver', 'volar', 'html', 'intelephense', 'cssls', 'jsonls', 'lua_ls' },
       handlers = {
         lsp_zero.default_setup,
         lua_ls = function()
