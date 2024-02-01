773a97e12673e34fbd4d5fb5a80945752e976123
BernardoBF4 <bernardo.fantin@gmail.com>
2024-01-31 23:23:03 -0300

    🆙 improve(tema: rosé pine): keyword com cores de minha preferência

--- lua/bernardo/plugins/rose-pine.lua
+++ lua/bernardo/plugins/rose-pine.lua
@@ -1,22 +1,27 @@
 return {
   'rose-pine/neovim',
   name = 'rose-pine',
-  config = function ()
-   require('rose-pine').setup({
-     highlight_groups = {
-      ["String"] = { fg = "#2bcf75"},
-      ["@type.qualifier"] = { fg = "pine" },
-      ["@variable"] = { fg = "#ffffff" },
-      ["@variable.builtin"] = { fg = "#ffffff" },
-      ["@parameter"] = { fg = "gold" },
-      ["@property"] = { fg = "#ffffff" },
-      ["@new_expression"] = { fg = "pine" },
-      ["@named_imports"] = { fg = "#ffffff" },
-      ["Operator"] = { fg = "pine" },
-      ["Function"] = { fg = "rose" },
-      ["Special"] = { fg = "rose" },
-      ["@constant.builtin"] = { fg = "gold" },
-     }
-   })
+  config = function()
+    require('rose-pine').setup({
+      styles = {
+        bold = false
+      },
+      highlight_groups = {
+        ["String"] = { fg = "#2bcf75" },
+        ["@type.qualifier"] = { fg = "pine" },
+        ["@variable"] = { fg = "#ffffff" },
+        ["@variable.builtin"] = { fg = "#ffffff" },
+        ["@parameter"] = { fg = "gold" },
+        ["@property"] = { fg = "#ffffff" },
+        ["@new_expression"] = { fg = "pine" },
+        ["@keyword.operator"] = { fg = "pine" },
+        ["@named_imports"] = { fg = "#ffffff" },
+        ["Operator"] = { fg = "pine" },
+        ["Function"] = { fg = "rose" },
+        ["@function.method.call"] = { fg = "rose" },
+        ["Special"] = { fg = "rose" },
+        ["@constant.builtin"] = { fg = "gold" },
+      }
+    })
   end
 }
