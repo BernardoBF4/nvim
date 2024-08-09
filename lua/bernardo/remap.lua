690c9d47c779f6241ea5ab11324876b2cdafc180
BernardoBF4 <bernardo.fantin@gmail.com>
2024-08-09 12:54:42 -0300

    🆙 improve(remaps: window): adicionados remaps para split de tela

--- lua/bernardo/remap.lua
+++ lua/bernardo/remap.lua
@@ -69,6 +69,10 @@ vim.keymap.set("n", "<leader>wj", "<C-w>j")
 vim.keymap.set("n", "<leader>wk", "<C-w>k")
 vim.keymap.set("n", "<leader>wh", "<C-w>h")
 vim.keymap.set("n", "<leader>wl", "<C-w>l")
+vim.keymap.set("n", "<leader>wv", "<C-w>v")
+vim.keymap.set("n", "<leader>ws", "<C-w>s")
+vim.keymap.set("n", "<leader>wc", "<C-w>c")
+vim.keymap.set("n", "<leader>wo", "<C-w>o")
 
 -- Conform
 vim.keymap.set(
