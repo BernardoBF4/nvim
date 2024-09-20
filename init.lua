4ea0ca24eab7cab6242ba8c35e6f76dcabe5961c
BernardoBF4 <bernardo.fantin@gmail.com>
2024-09-20 16:35:01 -0300

    🔥 cleanup(init): startup do nvim
    Removido código que não fazia nada e estava no lugar errado, sendo que
    habia uma duplicata em outro lugar

--- init.lua
+++ init.lua
@@ -2,6 +2,5 @@ require('bernardo.lazy')
 require('bernardo.options')
 require('bernardo.remap')
 require('bernardo.functions')
-require('bernardo.plugins.copilot-chat')
 
 vim.cmd("colorscheme rose-pine")
