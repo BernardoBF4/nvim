a05b538032631d26a2145377de5dc2216ff9e63f
BernardoBF4 <bernardo.fantin@gmail.com>
2024-09-20 16:44:48 -0300

    🗑️ remove(colour picker): remap para +5 ou -5
    Removido porque não seria usado

--- lua/bernardo/plugins/color-picker.lua
+++ lua/bernardo/plugins/color-picker.lua
@@ -4,10 +4,6 @@ return {
     require("color-picker").setup({
       ["icons"] = { "|", ">" },
       ["border"] = "rounded",
-      ["keymap"] = {
-        ["U"] = "<Plug>ColorPickerSlider5Decrease",
-        ["O"] = "<Plug>ColorPickerSlider5Increase",
-      },
       ["background_highlight_group"] = "Normal",
       ["border_highlight_group"] = "FloatBorder",
       ["text_highlight_group"] = "Normal",
