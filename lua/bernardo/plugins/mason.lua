3a6c4b092c7b9e24e6fc6b51df465c9cc08aba4a
BernardoBF4 <bernardo.fantin@gmail.com>
2024-11-23 09:36:23 -0300

    🆙 improve(lsp): volar
    Usando o volar no mode de take-over para o arquivos vue. Isso garante
    que mais funcionalidades de lsp e cmp irão ser empregadas no
    desenvolvimento

--- lua/bernardo/plugins/mason.lua
+++ lua/bernardo/plugins/mason.lua
@@ -16,7 +16,34 @@ return {
           lsp_zero.default_setup,
           lua_ls = function()
             local lua_opts = lsp_zero.nvim_lua_ls()
-            require('lspconfig').lua_ls.setup(lua_opts)
+            local lspconfig = require('lspconfig')
+            lspconfig.lua_ls.setup(lua_opts)
+            lspconfig.volar.setup({
+              filetypes = { 'javascriptreact', 'typescriptreact', 'vue' },
+              init_options = {
+                vue = {
+                  hybridMode = false,
+                },
+              },
+            })
+            lspconfig.ts_ls.setup({
+              init_options = {
+                plugins = {
+                  {
+                    name = '@vue/typescript-plugin',
+                    location = '/home/bernardo/.nvm/versions/node/v23.2.0/lib/node_modules/@vue/language-server',
+                    languages = { 'vue' },
+                  },
+                },
+              },
+            })
+            lspconfig.volar.setup({
+              init_options = {
+                vue = {
+                  hybridMode = false,
+                },
+              },
+            })
           end,
         }
       })
