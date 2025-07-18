65d5b9cfa3162526ccdb1cadf46aa2a6c5655c76
BernardoBF4 <bernardo.fantin@gmail.com>
2025-07-18 08:20:36 -0300

    🚑 fix(plugins): mason
    Servidor de linguagem do Vue e do TypeScript agora estão funcionando corretamente para as novas configurações do LSP.

--- lua/bernardo/plugins/mason.lua
+++ lua/bernardo/plugins/mason.lua
@@ -10,36 +10,58 @@ return {
     config = function()
       local lsp_zero = require('lsp-zero')
 
-      local lua_opts = lsp_zero.nvim_lua_ls()
-      vim.lsp.config('lua_ls', lua_opts)
+      vim.lsp.config('lua_ls', lsp_zero.nvim_lua_ls())
 
       vim.lsp.config('vue_ls', {
-        filetypes = { 'javascriptreact', 'typescriptreact', 'vue' },
-        init_options = {
-          vue = {
-            hybridMode = false,
-          },
-        },
-        on_attach = function(client)
-          client.server_capabilities.documentFormattingProvider = false
-          client.server_capabilities.documentRangeFormattingProvider = false
-        end
+        on_init = function(client)
+          client.handlers['tsserver/request'] = function(_, result, context)
+            local clients = vim.lsp.get_clients({ bufnr = context.bufnr, name = 'vtsls' })
+            if #clients == 0 then
+              vim.notify('Could not find `vtsls` lsp client, `vue_ls` would not work without it.', vim.log.levels.ERROR)
+              return
+            end
+            local ts_client = clients[1]
+
+            local param = unpack(result)
+            local id, command, payload = unpack(param)
+            ts_client:exec_cmd({
+              title = 'vue_request_forward', -- You can give title anything as it's used to represent a command in the UI, `:h Client:exec_cmd`
+              command = 'typescript.tsserverRequest',
+              arguments = {
+                command,
+                payload,
+              },
+            }, { bufnr = context.bufnr }, function(_, r)
+              local response_data = { { id, r.body } }
+              ---@diagnostic disable-next-line: param-type-mismatch
+              client:notify('tsserver/response', response_data)
+            end)
+          end
+        end,
       })
 
-      vim.lsp.config('ts_ls', {
-        init_options = {
-          plugins = {
-            {
-              name = '@vue/typescript-plugin',
-              location = vim.fn.expand("$HOME") .. "/.nvm/versions/node/v23.2.0/lib/node_modules/@vue/language-server",
-              languages = { 'vue' },
+      vim.lsp.config('vtsls', {
+        settings = {
+          vtsls = {
+            tsserver = {
+              globalPlugins = {
+                {
+                  name = '@vue/typescript-plugin',
+                  location = vim.fn.stdpath('data') ..
+                      "/mason/packages/vue-language-server/node_modules/@vue/language-server",
+                  languages = { 'vue' },
+                  configNamespace = 'typescript',
+                },
+              },
             },
           },
         },
+        filetypes = { 'typescript', 'javascript', 'javascriptreact', 'typescriptreact', 'vue' },
       })
+      vim.lsp.enable({ 'vtsls', 'vue_ls' })
 
       require('mason-lspconfig').setup({
-        ensure_installed = { 'ts_ls', 'vue_ls', 'html', 'intelephense', 'cssls', 'jsonls', 'lua_ls', 'pyright' },
+        ensure_installed = { 'vue_ls', 'html', 'intelephense', 'cssls', 'jsonls', 'lua_ls', 'pyright' },
         automatic_enable = true,
       })
     end
